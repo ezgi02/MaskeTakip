3bef8f70b108578fd6e762379c9e0170c81a6b72
Ezgi Adıgüzel <ezgiadiguzel01@gmail.com>
2024-02-12 17:04:08 +0300

    Add project files.

---
+++ Business/Concrete/PTTManager.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PTTManager
+    {
+        private IApplicantService _applicantService;
+        public void GiveMask(Person person)
+        {
+            
+            if (personManager.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + "için maske verildi");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿//See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//NewMethod();
+Vatandas vatandas1= new Vatandas();
+vatandas1.ad = "ahmet";
+
+Console.ReadLine();
+
+static void Degiskenler()
+{
+    string mesaj = "Merhaba";
+    double tutar = 100000;
+    int sayi = 100;
+    bool girisYapmisMi = false;
+
+
+    Console.WriteLine(tutar * 1.18);
+}
+
+class Vatandas
+{
+
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+}
+
